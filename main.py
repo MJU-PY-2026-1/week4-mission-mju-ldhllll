bfc4d8ec4b7f37fcbe71629c59284ed4740b4ea7
ldhllll <ldhllll@mju.ac.kr>
2026-03-31 12:09:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ bucket_list.append(input("맛집 리스트 입력:"))
 print(bucket_list, "\n")
 
 temp= input("맛집 리스트 입력:")
-bucket_list.insert(9, temp)
+bucket_list.insert(0, temp)
 print(bucket_list, "\n")
 
 temp = input("도장 깨기:")

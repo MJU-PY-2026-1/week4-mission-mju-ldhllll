50f54f5eefb0d0bf91ed2bb6202df44174b5a496
ldhllll <ldhllll@mju.ac.kr>
2026-03-31 12:08:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-# 파일이름 :
-# 작 성 자 :
+# 파일이름 : main.py
+# 작 성 자 : 다현
+
+bucket_list={}
+temp = "";
+
+bucket_list.append(input("맛집 리스트 입력:"))
+
+bucket_list.append(input("맛집 리스트 입력:"))
+
+bucket_list.append(input("맛집 리스트 입력:"))
+print(bucket_list, "\n")
+
+temp= input("맛집 리스트 입력:")
+bucket_list.insert(9, temp)
+print(bucket_list, "\n")
+
+temp = input("도장 깨기:")
+bucket_list.remove(temp)
+print(bucket_list)
